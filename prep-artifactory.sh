661330827d5409043df2c9f65a41bdc161ffab69
Larry Presswood <larry.presswood@regions.com>
2019-05-13 12:54:18 -0400

    Latest modifications after testing

--- prep-artifactory.sh
+++ prep-artifactory.sh
@@ -7,17 +7,20 @@
 sudo yum update -y
 sudo yum install wget unzip zip -y
 sudo yum install java-1.8.0-openjdk -y
+# Install Artifactory
 sudo wget https://bintray.com/jfrog/artifactory-rpms/download_file?file_path=jfrog-artifactory-oss-6.10.0.rpm -O jfrog-artifactory-oss-6.10.0.rpm
-sudo yum install jfrog-artifactory-oss-6.10.0.rpm
+sudo yum install /home/ec2-user/jfrog-artifactory-oss-6.10.0.rpm -y
 echo 'export ARTIFACTORY_HOME=/opt/jfrog/artifactory' | sudo tee -a /etc/profile
+sudo rm /home/ec2-user/jfrog-artifactory-oss-6.10.0.rpm
 # Install MariaDB for Artifactory
 sudo yum install -y mariadb-server
 sudo systemctl start mariadb.service
 sudo systemctl enable mariadb.service
 # Install Ansible
-#Install Ansible
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install ansible -y
+# install pexpect
+sudo easy_install pexpect
 
 
 
